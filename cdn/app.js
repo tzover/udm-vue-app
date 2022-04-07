001e708d71318704ab82ac8cace25f494eedf82f
tzover <tzover.68@gmail.com>
2022-04-07 22:06:36 +0900

    update v-bind and mouseEvents

--- cdn/app.js
+++ cdn/app.js
@@ -2,11 +2,24 @@
   const app = Vue.createApp({
     data() {
       return {
+        url: 'https://google.com',
         showDetails: true,
         title: 'The final empire',
         auther: 'yt',
         age: 28,
         count: 0,
+        x: 0,
+        y: 0,
+        books: [
+          { title: 'sample1', data: 'data1', img: 'assets/1.jpg', isFav: true },
+          {
+            title: 'sample2',
+            data: 'data2',
+            img: 'assets/2.jpg',
+            isFav: false,
+          },
+          { title: 'sample3', data: 'data3', img: 'assets/3.jpg', isFav: true },
+        ],
       }
     },
     methods: {
@@ -17,6 +30,16 @@
       },
       changeStatus() {
         this.showDetails = !this.showDetails
+      },
+      handleEvent(e, data) {
+        console.log(e)
+        if (data) {
+          console.log(data)
+        }
+      },
+      handleMousemove(e) {
+        this.x = e.offsetX
+        this.y = e.offsetY
       },
     },
   })

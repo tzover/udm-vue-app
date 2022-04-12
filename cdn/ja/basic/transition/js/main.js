e247083d867d67ff8524a5d2576ffecad005a8ff
tzover <tzover.68@gmail.com>
2022-04-13 02:27:44 +0900

    create transition sample and complete js course

---
+++ cdn/ja/basic/transition/js/main.js
@@ -0,0 +1,13 @@
+// This is a template
+{
+  const app = Vue.createApp({
+    data() {
+      return {
+        show: true,
+      }
+    },
+    methods: {},
+    computed: {},
+  })
+  app.mount('#app')
+}

af043ae06536492cd63e9476778ded275394dca4
tzover <tzover.68@gmail.com>
2022-04-14 01:43:56 +0900

    create new project about reaction-timer

---
+++ app/reaction-timer/src/components/Block.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="block" v-if="showBlock">click me</div>
+</template>
+
+<script>
+export default {
+  props: ['delay'],
+  data() {
+    return {
+      showBlock: false,
+    }
+  },
+  mounted() {
+    console.log('compoent mounted')
+    setTimeout(() => {
+      this.showBlock = true
+      console.log(this.delay)
+    }, this.delay)
+  },
+  // updated() {
+  //   console.log('component updated')
+  // },
+  // unmounted() {
+  //   console.log('unmounted')
+  // },
+}
+</script>
+
+<style>
+.block {
+  width: 400px;
+  border-radius: 20px;
+  background: #0faf87;
+  color: white;
+  text-align: center;
+  padding: 100px 0;
+  margin: 40px auto;
+}
+</style>

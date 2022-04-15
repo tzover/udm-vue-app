d7aa5b3dfdea14471370f30ea97b93d8f5855c13
tzover <tzover.68@gmail.com>
2022-04-15 15:10:39 +0900

    create new project about ninja-jobs -> fetch sample

---
+++ app/ninja-jobs/src/views/jobs/JobDetails.vue
@@ -0,0 +1,31 @@
+<template>
+  <h1>Job Details Page</h1>
+  <div v-if="job">
+    <h2>{{ job.title }}</h2>
+    <p>The Job id is {{ id }}</p>
+    <p>{{ job.details }}</p>
+  </div>
+  <div v-else>
+    <p>Loading job details...</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['id'],
+  data() {
+    return {
+      // id: this.$route.params.id,
+      job: null,
+    }
+  },
+  mounted() {
+    fetch('http://localhost:8888/jobs/' + this.id)
+      .then((res) => res.json())
+      .then((data) => (this.job = data))
+      .catch((err) => console.log(err.message))
+  },
+}
+</script>
+
+<style></style>

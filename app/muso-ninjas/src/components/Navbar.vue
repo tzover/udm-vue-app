95507cebc15335105a151f7224a14314cfa13a01
tzover <tzover.68@gmail.com>
2022-04-21 14:26:02 +0900

    create new project about muso-ninjas

---
+++ app/muso-ninjas/src/components/Navbar.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="navbar">
+    <nav>
+      <img src="@/assets/ninja.png" />
+      <h1><router-link :to="{ name: 'Home' }">Muso Ninjas</router-link></h1>
+      <div class="links">
+        <div v-if="user">
+          <button @click="handleClick">Logout</button>
+        </div>
+        <div v-else>
+          <router-link class="btn" :to="{ name: 'Signup' }">Signup</router-link>
+          <router-link class="btn" :to="{ name: 'Login' }">Login</router-link>
+        </div>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+import { useRouter } from 'vue-router'
+import getUser from '../composables/getUser'
+import useLogout from '../composables/useLogout'
+
+export default {
+  setup() {
+    const { user } = getUser()
+    const { logout } = useLogout()
+    const router = useRouter()
+
+    const handleClick = async () => {
+      await logout()
+      console.log('logged out')
+      router.push({ name: 'Login' })
+    }
+
+    return { handleClick, user }
+  },
+}
+</script>
+
+<style scoped>
+.navbar {
+  padding: 16px 10px;
+  margin-bottom: 60px;
+  background: white;
+}
+nav {
+  display: flex;
+  align-items: center;
+  max-width: 1200px;
+  margin: 0 auto;
+}
+nav img {
+  max-height: 60px;
+}
+nav h1 {
+  margin-left: 20px;
+}
+nav .links {
+  margin-left: auto;
+}
+nav .links a,
+button {
+  margin-left: 16px;
+  font-size: 14px;
+}
+</style>
---
+++ app/muso-ninjas/src/router/index.js
@@ -0,0 +1,35 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from '../views/Home.vue'
+import Login from '../views/auth/Login.vue'
+import Signup from '../views/auth/Signup.vue'
+import CreatePlaylist from '../views/playlists/CreatePlaylist.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home,
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login,
+  },
+  {
+    path: '/signup',
+    name: 'Signup',
+    component: Signup,
+  },
+  {
+    path: '/playlists/create',
+    name: 'CreatePlaylist',
+    component: CreatePlaylist,
+  },
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes,
+})
+
+export default router

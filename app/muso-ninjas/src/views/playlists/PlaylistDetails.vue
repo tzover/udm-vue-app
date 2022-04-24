1147d3661e08e2ca0bcbe3eae3346fc504b7ce27
tzover <tzover.68@gmail.com>
2022-04-24 23:20:55 +0900

    modified and created some vue file

--- app/muso-ninjas/src/views/playlists/PlaylistDetails.vue
+++ app/muso-ninjas/src/views/playlists/PlaylistDetails.vue
@@ -1,9 +1,89 @@
-<template></template>
+<template>
+  <div v-if="error" class="error">{{ error }}</div>
+  <div v-if="playlist" class="playlist-details">
+    <!-- Playlist information -->
+    <div class="playlist-info">
+      <div class="cover">
+        <img :src="playlist.coverUrl" />
+      </div>
+      <h2>{{ playlist.title }}</h2>
+      <p class="username">Created by {{ playlist.userName }}</p>
+      <p class="desctiption">{{ playlist.desctiption }}</p>
+      <button v-if="ownership" @click="handleDelete">Delete Playlist</button>
+    </div>
+
+    <!-- song list -->
+    <div class="song-list">
+      <p></p>
+    </div>
+  </div>
+</template>
 
 <script>
+import { computed } from 'vue'
+import getDocument from '@/composables/getDocument'
+import getUser from '@/composables/getUser'
+import useDocument from '@/composables/useDocument'
+
 export default {
   props: ['id'],
+  setup() {
+    const { error, document: playlist } = getDocument('playlists', props.id)
+    const { user } = getUser()
+    const { deleteDoc } = useDocument('playlists', props.id)
+
+    const ownership = computed(() => {
+      return (
+        playlist.value && user.value && user.value.uid === playlist.value.userid
+      )
+    })
+
+    const handleDelete = async () => {
+      await deleteDoc()
+    }
+
+    return { error, playlist, ownership, handleDelete }
+  },
 }
 </script>
 
-<style></style>
+<style>
+.playlist-details {
+  display: grid;
+  grid-template-columns: 1fr 2fr;
+  gap: 80px;
+}
+.cover {
+  overflow: hidden;
+  border-radius: 20px;
+  position: relative;
+  padding: 160px;
+}
+.cover img {
+  display: block;
+  position: absolute;
+  top: 0;
+  left: 0;
+  min-width: 100%;
+  min-height: 100%;
+  max-width: 200%;
+  max-height: 200%;
+}
+.playlist-info {
+  text-align: center;
+}
+.playlist-info h2 {
+  text-transform: capitalize;
+  font-size: 28px;
+  margin-top: 20px;
+}
+.playlist-info p {
+  margin-bottom: 20px;
+}
+.username {
+  color: #999;
+}
+.description {
+  text-align: left;
+}
+</style>
